7be81041f03c3aedb02d74da0f440134ca088453
rpf1980 <rafapolo1980@gmail.com>
2019-10-28 09:15:00 +0100

    chore(): minor change

---
+++ AppPizeria_v02/app/src/main/java/com/example/apppizeria_v02/Activity_personalizada.java
@@ -0,0 +1,16 @@
+package com.example.apppizeria_v02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_personalizada extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personalizada);
+    }
+}
