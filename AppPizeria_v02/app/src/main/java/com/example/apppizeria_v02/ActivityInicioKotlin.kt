32560ba846d1784cd2c099f0cc4273d90b66dd58
rpf1980 <united_ssh@outlook.com>
2019-11-03 17:55:25 +0100

    chore(): add file Kotlin

---
+++ AppPizeria_v02/app/src/main/java/com/example/apppizeria_v02/ActivityInicioKotlin.kt
@@ -0,0 +1,12 @@
+package com.example.apppizeria_v02
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityInicioKotlin : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inicio_kotlin)
+    }
+}
