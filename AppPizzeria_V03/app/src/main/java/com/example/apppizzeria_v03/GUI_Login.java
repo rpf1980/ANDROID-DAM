96a8b4e06337ab864b560622fcd46f8a41bb86cd
rpf1980 <united_ssh@outlook.com>
2019-11-04 23:12:26 +0100

    chore(): minor changes

---
+++ AppPizzeria_V03/app/src/main/java/com/example/apppizzeria_v03/GUI_Login.java
@@ -0,0 +1,16 @@
+package com.example.apppizzeria_v03;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GUI_Login extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gui__login);
+    }
+}
