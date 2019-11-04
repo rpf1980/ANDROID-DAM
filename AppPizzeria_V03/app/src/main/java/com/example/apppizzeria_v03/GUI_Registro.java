f4c88ca1a90180896039b7ecca9921aee3f7be1c
rpf1980 <united_ssh@outlook.com>
2019-11-04 09:30:51 +0100

    feat(): add GuiRegistro

--- AppPizzeria_V03/app/src/main/java/com/example/apppizzeria_v03/GUI_Registro.java
+++ AppPizzeria_V03/app/src/main/java/com/example/apppizzeria_v03/GUI_Registro.java
@@ -3,14 +3,29 @@ package com.example.apppizzeria_v03;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class GUI_Registro extends AppCompatActivity
 {
+
+    // Componentes de la GUI
+    EditText edtUsuarioGuiRegistro;
+    EditText edtEmailGuiRegistro;
+    EditText edtPassGuiRegistro;
+    Button btnGuiRegistro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gui__registro);
+
+
+
+
     }
+
+    // Método que enlaza los id's con los componentes de la app
+
 }
--- AppPizzeria_V03/app/src/main/java/com/example/apppizzeria_v03/MainActivity.java
+++ AppPizzeria_V03/app/src/main/java/com/example/apppizzeria_v03/MainActivity.java
@@ -2,15 +2,39 @@ package com.example.apppizzeria_v03;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity
 {
+
+    // Componentes de la App
+    Button btnInciarSesion;
+    Button btnRegistrarse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Asociamos los id's
+        btnInciarSesion = findViewById(R.id.idBtnInitSesionEnInicio);
+        btnRegistrarse = findViewById(R.id.btnRegistraseEnInicio);
+    }
+
+    // Evento INICIAR SESIÓN
+    public void clickIniciarSesionGuiInicio(View v)
+    {
+
+    }
+
+    // Evento REGISTRARSE
+    public void clickRegistrarseGuiInicio(View v)
+    {
+        Intent intent = new Intent(getApplicationContext(), GUI_Registro.class);
+        startActivity(intent);
     }
 }
